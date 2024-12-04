dc29c460e937a6237eb2946430d0d5d82e884b5c
DerhundVonEdengart <s40547@bj.ac.th>
2024-12-04 13:30:42 +0700

    Add files via upload

---
+++ a3_sheep_06/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1733293877,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sheep-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/tiledbackground.png",
+		"images/pillar.png",
+		"images/land-sheet0.png",
+		"images/line.png",
+		"images/point-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
